768ca3e543400c21886fa7d19b87235a2f38a879
Martin Lillepuu <martin@mlp.ee>
2021-06-26 18:00:16 +0300

    remove duplicate GET_ADDR

--- board/safety/safety_subaru.h
+++ board/safety/safety_subaru.h
@@ -210,7 +210,6 @@ static int subaru_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
       // 0x221 ES_Distance
       // 0x321 ES_DashStatus
       // 0x322 ES_LKAS_State
-      int addr = GET_ADDR(to_fwd);
       int block_msg = ((addr == 0x122) || (addr == 0x221) || (addr == 0x321) || (addr == 0x322));
       if (!block_msg) {
         bus_fwd = 0;  // Main CAN
