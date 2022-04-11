e26a2bad94e8385cb8b4c844c5b34bfa2b77e4a9
Martin Lillepuu <martin@mlp.ee>
2022-04-12 00:18:04 +0300

    update subaru_gen2_tx_hook

--- board/safety/safety_subaru.h
+++ board/safety/safety_subaru.h
@@ -420,7 +420,9 @@ static int subaru_gen2_rx_hook(CANPacket_t *to_push) {
   return valid;
 }
 
-static int subaru_gen2_tx_hook(CANPacket_t *to_send) {
+static int subaru_gen2_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
+  UNUSED(longitudinal_allowed);
+
   int tx = 1;
   int addr = GET_ADDR(to_send);
 
