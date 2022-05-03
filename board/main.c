1c19ed18ea2af15454a2e409465ce4eb5398a619
Jason Wen <haibin.wen3@gmail.com>
2022-05-03 18:24:42 -0400

    Little endian says no

--- board/main.c
+++ board/main.c
@@ -716,11 +716,11 @@ void TIM1_BRK_TIM9_IRQ_Handler(void) {
       void send_id(uint8_t obj_valid, uint8_t acc_obj_lat_pos_1, uint8_t acc_obj_lat_pos_2, uint8_t acc_obj_dist_1, uint8_t acc_obj_dist_2, uint8_t acc_obj_rel_spd_1, uint8_t acc_obj_rel_spd_2) {
         uint8_t dat[8];
         dat[0] = 0x00;
-        dat[1] = (acc_obj_rel_spd_2 >> 8);
-        dat[2] = (acc_obj_rel_spd_1) | (acc_obj_dist_2 >> 4);
-        dat[3] = (acc_obj_dist_1) | (acc_obj_lat_pos_2 >> 1);
+        dat[1] = (acc_obj_rel_spd_2);
+        dat[2] = (acc_obj_rel_spd_1 >> 4) | (acc_obj_dist_2 << 4);
+        dat[3] = (acc_obj_dist_1 >> 1) | (acc_obj_lat_pos_2 << 7);
         dat[4] = (acc_obj_lat_pos_1);
-        dat[5] = (obj_valid >> 1);
+        dat[5] = (obj_valid);
         dat[6] = 0x00;
         dat[7] = 0x00;
         CAN1->sTxMailBox[0].TDLR = dat[0] | (dat[1] << 8) | (dat[2] << 16) | (dat[3] << 24);
