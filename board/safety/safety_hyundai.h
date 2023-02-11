85cf3fee86f0cb860b24228a3bdebafc50d41a19
Jason Wen <haibin.wen3@gmail.com>
2023-02-11 00:32:40 -0500

    HKG: Car Port for 2023 Hyundai Palisade & Kia Telluride

--- board/safety/safety_hyundai.h
+++ board/safety/safety_hyundai.h
@@ -60,6 +60,14 @@ AddrCheckStruct hyundai_addr_checks[] = {
 };
 #define HYUNDAI_ADDR_CHECK_LEN (sizeof(hyundai_addr_checks) / sizeof(hyundai_addr_checks[0]))
 
+AddrCheckStruct hyundai_can_canfd_addr_checks[] = {
+  {.msg = {{608, 0, 8, .check_checksum = true, .max_counter = 3U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+  {.msg = {{902, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{916, 0, 8, .check_checksum = true, .max_counter = 7U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{1057, 0, 8, .max_counter = 14U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
+};
+#define HYUNDAI_CAN_CANFD_ADDR_CHECK_LEN (sizeof(hyundai_can_canfd_addr_checks) / sizeof(hyundai_can_canfd_addr_checks[0]))
+
 AddrCheckStruct hyundai_cam_scc_addr_checks[] = {
   {.msg = {{608, 0, 8, .check_checksum = true, .max_counter = 3U, .expected_timestep = 10000U},
            {881, 0, 8, .expected_timestep = 10000U}, { 0 }}},
@@ -88,7 +96,10 @@ AddrCheckStruct hyundai_legacy_addr_checks[] = {
 };
 #define HYUNDAI_LEGACY_ADDR_CHECK_LEN (sizeof(hyundai_legacy_addr_checks) / sizeof(hyundai_legacy_addr_checks[0]))
 
+const int HYUNDAI_PARAM_CAN_CANFD = 128;
+
 bool hyundai_legacy = false;
+bool hyundai_can_canfd = false;
 
 addr_checks hyundai_rx_checks = {hyundai_addr_checks, HYUNDAI_ADDR_CHECK_LEN};
 
@@ -177,8 +188,13 @@ static int hyundai_rx_hook(CANPacket_t *to_push) {
 
   // SCC12 is on bus 2 for camera-based SCC cars, bus 0 on all others
   if (valid && (addr == 1057) && (((bus == 0) && !hyundai_camera_scc) || ((bus == 2) && hyundai_camera_scc))) {
-    // 2 bits: 13-14
-    int cruise_engaged = (GET_BYTES_04(to_push) >> 13) & 0x3U;
+    int cruise_engaged = 0;
+    if (hyundai_can_canfd) {
+      cruise_engaged = (GET_BYTE(to_push, 3) >> 4) & 0x3U;
+    } else {
+      // 2 bits: 13-14
+      cruise_engaged = (GET_BYTES_04(to_push) >> 13) & 0x3U;
+    }
     hyundai_common_cruise_state_check(cruise_engaged);
   }
 
@@ -324,8 +340,9 @@ static int hyundai_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
 static const addr_checks* hyundai_init(uint16_t param) {
   hyundai_common_init(param);
   hyundai_legacy = false;
+  hyundai_can_canfd = GET_FLAG(param, HYUNDAI_PARAM_CAN_CANFD);
 
-  if (hyundai_camera_scc) {
+  if (hyundai_camera_scc || hyundai_can_canfd) {
     hyundai_longitudinal = false;
   }
 
@@ -333,6 +350,8 @@ static const addr_checks* hyundai_init(uint16_t param) {
     hyundai_rx_checks = (addr_checks){hyundai_long_addr_checks, HYUNDAI_LONG_ADDR_CHECK_LEN};
   } else if (hyundai_camera_scc) {
     hyundai_rx_checks = (addr_checks){hyundai_cam_scc_addr_checks, HYUNDAI_CAM_SCC_ADDR_CHECK_LEN};
+  } else if (hyundai_can_canfd) {
+    hyundai_rx_checks = (addr_checks){hyundai_can_canfd_addr_checks, HYUNDAI_CAN_CANFD_ADDR_CHECK_LEN};
   } else {
     hyundai_rx_checks = (addr_checks){hyundai_addr_checks, HYUNDAI_ADDR_CHECK_LEN};
   }
