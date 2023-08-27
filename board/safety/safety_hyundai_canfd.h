8753ad0b47f102be4536f91d6094ea9f80e45549
Jason Wen <haibin.wen3@gmail.com>
2023-08-26 20:43:50 -0400

    hda2 long doesn't need to send 0x1CF

--- board/safety/safety_hyundai_canfd.h
+++ board/safety/safety_hyundai_canfd.h
@@ -32,7 +32,6 @@ const CanMsg HYUNDAI_CANFD_HDA2_ALT_STEERING_TX_MSGS[] = {
 
 const CanMsg HYUNDAI_CANFD_HDA2_LONG_TX_MSGS[] = {
   {0x110, 0, 32}, // LKAS_ALT
-  {0x1CF, 1, 8},  // CRUISE_BUTTON
   {0x362, 0, 32}, // CAM_0x362
   {0x730, 1, 8},  // tester present for ADAS ECU disable
   {0x12A, 1, 16}, // LFA
